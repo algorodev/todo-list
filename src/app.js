47b23e67f6d3a8b615bf9e0083899c1776b11992
Alejandro Gonzalez Romero <me@algorodev.com>
2023-08-31 17:58:01 +0200

    refactor: app functionality

--- src/app.js
+++ src/app.js
@@ -1,44 +1,38 @@
 import { createTodo, getTodos } from './services/todo.service'
+import * as element from './utils/elements'
 
-const titleFormControl = document.getElementById('form_input')
-const formButton = document.getElementById('form_button')
-const listContainer = document.getElementById('list_container')
-const listLength = document.getElementById('todo_list_length')
-const todoForm = document.getElementById('todo_form')
+let todoHTMLElements = ''
+let todoItemsLength = 0
 
-todoForm.addEventListener('submit', (event) => {
+element.todoForm.addEventListener('submit', (event) => onCreateTodo(event))
+
+element.formButton.addEventListener('click', (event) => onCreateTodo(event))
+
+export const onCreateTodo = (event) => {
 	event.preventDefault()
-	if (titleFormControl.value === '') return
+	if (element.titleFormControl.value === '') return
 
-	createTodo(titleFormControl.value)
-	titleFormControl.value = null
-
-	loadTodoList()
-})
-
-formButton.addEventListener('click', () => {
-	if (titleFormControl.value === '') return
-
-	createTodo(titleFormControl.value)
-	titleFormControl.value = null
+	createTodo(element.titleFormControl.value)
+	element.titleFormControl.value = null
 
 	loadTodoList()
-})
+}
 
-export const refreshList = (list, index) => {
-	listContainer.innerHTML = list
-	listLength.textContent = index
+export const refreshList = () => {
+	element.listContainer.innerHTML = todoHTMLElements
+	element.listLength.textContent = todoItemsLength + ''
+	todoHTMLElements = ''
 }
 
 export const loadTodoList = () => {
 	const todoList = getTodos()
-	let listItems = ''
 
 	todoList.map((todo) => {
-		listItems += `<div class="todo-item"><p id="todo_item_${todo.id}">${todo.title}</p><button id="todo_item_${todo.id}_delete_button" class="todo-item__action"><label for="todo_item_${todo.id}_delete_button" class="todo-item__label-action material-symbols-outlined" onclick="deleteTodoById(${todo.id})">delete</label></button></div>`
+		++todoItemsLength
+		todoHTMLElements += `<div class="todo-item"><p id="todo_item_${todo.id}">${todo.title}</p><button id="todo_item_${todo.id}_delete_button" class="todo-item__action"><label for="todo_item_${todo.id}_delete_button" class="todo-item__label-action material-symbols-outlined" onclick="deleteTodoById(${todo.id})">delete</label></button></div>`
 	})
 
-	refreshList(listItems, todoList.length)
+	refreshList()
 }
 
 loadTodoList()
---
+++ src/utils/elements.js
@@ -0,0 +1,9 @@
+export const titleFormControl = document.getElementById('form_input')
+
+export const formButton = document.getElementById('form_button')
+
+export const listContainer = document.getElementById('list_container')
+
+export const listLength = document.getElementById('todo_list_length')
+
+export const todoForm = document.getElementById('todo_form')
